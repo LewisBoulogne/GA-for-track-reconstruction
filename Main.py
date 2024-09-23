ade95775f08c0e6f8c22950a315c9e566e339e74
Lewis Boulogne <lewis.boulogne03@gmail.com>
2024-09-23 15:40:37 +0200

    adding gitignore and main changes

--- Main.py
+++ Main.py
@@ -3,106 +3,138 @@ import matplotlib.pyplot as plt
 from random import randint
 
 """
-LEVEL 1: generate tracks randomly until they correspond to the solution
+Simplified case:
+
+2 tracks 3 layers
+
+hits:
+
+h1 - h3 - h5
+h2 - h4 - h6
+
+segments:
+
+ . s1 . s5 .
+   \s2  \s6
+   /s3  /s7
+ . s4 . s8 .
+
+individual = binar string [s1, s2, s3, s4, s5, s6, s7, s8]
+
 """
 
-generation = 0
-tracks = []
-solution = [
-    [(0, 0), (0, 1), (0, 2)],
-    [(1, 0), (1, 1), (1, 2)]
-]
-
+solution = "100110011001"
+all_scores = []
+population_size = 10
+os_amount = int(population_size/3)
+runs = 3
 
 def init():
-    global tracks
-    for r in range(2):
-        tracks.append([(randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2))])
+    tracks = []
+    for r in range(population_size):
+        ind = ""
+        for i in range(len(solution)):
+            ind += str(randint(0,1))
+        tracks.append(ind)
+    return tracks
 
 
-def generate():
-    removed = 0
-    for c in solution:
-        for r in tracks:
-            if r != c:
-                tracks.remove(r)
-                removed += 1
-    for i in range(removed):
-        tracks.append([(randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2))])
-
-
-def found():
+def found(popu):
     # check whether the tracks correspond to solution
-    global tracks, solution
-    if tracks == solution:
+    global solution
+    if solution in popu:
         return True
     else:
         return False
 
 
+def get_fit(population):
+    global solution, mean_score
+    scores = []
+    for ind in population:
+        score = 0
+        for i in range(len(solution)):
+            if ind[i-1] == solution[i-1]:
+                score += 1
+        scores.append((ind, score))
+    sum = 0
+    for tuple in scores:
+        sum += tuple[1]
+    av_score = sum / len(population)
+    fittest = []
+    for tup in scores:
+        if tup[1] >= av_score:
+            fittest.append(tup[0])
+    if len(fittest) != int(len(population)/2):
+        if len(fittest) < int(len(population)/2):
+            for r in range(int(len(population)/2) - len(fittest)):
+                indiv = ""
+                for j in range(len(solution)):
+                    indiv += str(randint(0,1))
+                fittest.append(indiv)
+        if len(fittest) > int(len(population)/2):
+            for c in range(len(fittest) - int(len(population)/2)):
+                rand_index = randint(0, len(fittest)-1)
+                fittest.pop(rand_index)
+    mean_score.append(av_score)
+    return fittest
+
+def os_pop(parents): #off-springs
+    global solution, os_amount
+    next_gen = []
+    for par in range(os_amount):
+        if par%2 == 0:
+            p1 = parents[par]
+            p2 = parents[par - 1]
+            cross_point = randint(0, len(solution) - 1)
+            os1 = p1[0: cross_point] + p2[cross_point: len(solution)]
+            os2 = p2[0: cross_point] + p1[cross_point: len(solution)]
+            next_gen.append(os1)
+            next_gen.append(os2)
+    return next_gen
+
+def next_pop(population):
+    next_pop = []
+    fittest = get_fit(population)
+    for fit in fittest:
+        next_pop.append(fit)
+    offspring = os_pop(fittest)
+    for os in offspring:
+        next_pop.append(os)
+    if len(next_pop) != len(population):
+        if len(next_pop) < len(population):
+            for i in range(len(population) - len(next_pop)):
+                ind = ""
+                for i in range(len(solution)):
+                    ind += str(randint(0,1))
+                next_pop.append(ind)
+        if len(next_pop) > len(population):
+            for j in range(len(next_pop) - len(population)):
+                rand_index = randint(0, len(next_pop)-1)
+                next_pop.pop(rand_index)
+    return next_pop
+    
+
 def main_lvl1():
-    global generation
-    generation = 1
-    solve = False
-    init()
-    while not solve:
-        if found():
-            solve = True
-            print("!!!SOLVED!!!")
-            print("After " + str(generation) + " generations")
-            print("solution")
-            for c in solution:
-                print(c)
-            print("tracks")
-            for i in tracks:
-                print(i)
-
-        else:
-            print("gen " + str(generation))
-            generate()
+    global mean_score, all_scores, runs
+    for r in range(runs):
+        mean_score = []
+        generation = 1
+        pop = init()
+        print(pop)
+        while not found(pop) and generation < 100:
+            print(generation)
+            pop = next_pop(pop)
+            print(pop)
             generation += 1
-            print("solution")
-            for c in solution:
-                print(c)
-            print("tracks")
-            for i in tracks:
-                print(i)
+        if found(pop):
+            print("Solved in " + str(generation) + " generations")
+        if generation == 100:
+            print("Generation limit reached")
+        all_scores.append(mean_score)
+    for score_list in all_scores:
+        plt.plot(score_list)
+    plt.show()
+    
 
-
-"""
-LEVEL 2: generate 10 tracks and keep the tracks that correspond to a track from the solution
-"""
-
-def initialize():
-    global tracks
-    for r in range(10):
-        tracks.append([(randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2)),
-                       (randint(0, 2), randint(0, 2))])
-
-def evaluate(num):
-    global tracks, solution
-    new_tracks = []
-    removed = 0
-    if num == 0:
-        for i in solution:
-            for j in tracks:
-                if j == i:
-                    new_tracks.append(j)
-                else:
-                    tracks.remove(j)
-                    removed += 1
-
-
-
-def main_lvl2():
-    global generation
-    generation = 1
-    solved = 0
-    init()
-    while solved != 2:
-        evaluate(solved)
+main_lvl1()
